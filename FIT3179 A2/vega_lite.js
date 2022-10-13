1372eb1e99d3fee44d578dc0baf89c41be768792
0507huiyi <113984357+0507huiyi@users.noreply.github.com>
2022-10-13 12:35:18 +1100

    Add files via upload

---
+++ FIT3179 A2/vega_lite.js
@@ -0,0 +1,10 @@
+var vg_1 = "graph1.json"
+var vg_2 = "graph2.json"
+
+vegaEmbed('#first_chart', vg_1).then(function (result) {
+
+}).catch(console.error);
+
+vegaEmbed('#second_chart', vg_2).then(function (result) {
+
+}).catch(console.error);
